fc4d7bb03342c07399ea03a7d497867db01c7d41
externaltools <108364510+externaltools@users.noreply.github.com>
2022-07-05 14:10:35 -0400

    Add files via upload

---
+++ bad.bat
@@ -0,0 +1,10 @@
+wmic bios >> wmic.out
+wmic bootconfig >> wmic.out
+wmic cpu >> wmic.out
+wmic desktop >> wmic.out
+wmic desktopmonitor >> wmic.out
+wmic environment >> wmic.out
+wmic logicaldisk >> wmic.out
+wmic netclient >> wmic.out
+wmic ntdomain >>wmic.out
+wmic useraccount get AccountType >> wmic.out
